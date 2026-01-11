e781632f8c05667ed9dd0fab6821022cfc3cdcfc
Craig Robinson <craigrobinson2@gmail.com>
2026-01-10 20:32:26 -0800

    v2.2.2: Charter link fix + deployment verification

--- src/cfb_bot/utils/version_manager.py
+++ src/cfb_bot/utils/version_manager.py
@@ -11,7 +11,7 @@ from datetime import datetime
 logger = logging.getLogger('CFB26Bot.Version')
 
 # Current version
-CURRENT_VERSION = "2.2.1"
+CURRENT_VERSION = "2.2.2"
 
 # Changelog - organized by version
 CHANGELOG: Dict[str, Dict] = {
